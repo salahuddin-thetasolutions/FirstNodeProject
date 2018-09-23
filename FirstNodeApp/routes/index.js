e14df4e11ebc65f6df9bbb90d59b4f066c10f4b9
HAFIZ SALAH UD DIN <salahudinhafiz@hotmail.com>
2018-09-23 18:34:27 +0500

    Add project files.

---
+++ FirstNodeApp/routes/index.js
@@ -0,0 +1,22 @@
+﻿'use strict';
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function (req, res) {
+    res.render('index', { title: 'Express' });
+});
+
+
+/* GET Contact page. */
+router.get('/Contact', function (req, res) {
+    res.render('contact', { title: 'Express' });
+});
+
+
+/* GET about us page. */
+router.get('/aboutus', function (req, res) {
+    res.render('aboutus', { title: 'Express' });
+});
+
+module.exports = router;
